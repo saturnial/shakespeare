60a98f540ed74020fd02440e07b5aab950457d2f
Kayvon Tehranian <ktehranian@gmail.com>
2014-01-26 15:44:04 -0800

    Updated models and activated remote api

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import cgi
 import os
 import urllib
 
-from google.appengine.api import users
 from google.appengine.ext import ndb
+from google.appengine.api import users
 
 import jinja2
 import webapp2
+
+import models
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -16,8 +19,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
 
   def get(self):
+    template_values = {'result': models.Word.query()}
     template = JINJA_ENVIRONMENT.get_template('index.html')
-    template_values = {}
     self.response.write(template.render(template_values))
 
 
@@ -25,8 +28,14 @@ class Search(webapp2.RequestHandler):
 
   def get(self):
     query = self.request.get('query')
+    sentence1 = models.Sentence(book="Waiting For The Barbarians", sentence="Pain is truth; all else is subject to doubt.")
+    sentence2 = models.Sentence(book="Heart of Darkness", sentence="Your strength is just an accident owed to the weakness of others.")
+    word = models.Word(word=query, sentences=[sentence1, sentence2])
+    word.put()
+    self.redirect('/')
 
 
 application = webapp2.WSGIApplication([
+    ('/search', Search),
     ('/', MainPage),
 ], debug=True)
--- models.py
+++ models.py
@@ -1,9 +1,13 @@
 from google.appengine.ext import ndb
 
 
+class Sentence(ndb.Model):
+  sentence = ndb.StringProperty(required=True)
+  book = ndb.StringProperty(required=True)
+
+
 class Word(ndb.Model):
   word = ndb.StringProperty(required=True)
-  sentence = ndb.StringProperty(repeated=True)
-  book = ndb.StringProperty(required=True)
+  sentences = ndb.StructuredProperty(Sentence, repeated=True)
 
 
