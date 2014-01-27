de83e0a298dc455078469f30fc769ee57d77fbbc
Kayvon Tehranian <ktehranian@gmail.com>
2014-01-26 16:49:04 -0800

    Adding blobstore handler

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import cgi
 import os
 import urllib
 
-from google.appengine.ext import ndb
-from google.appengine.api import users
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 
 import jinja2
 import webapp2
@@ -15,6 +15,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
 
 class MainPage(webapp2.RequestHandler):
 
@@ -33,7 +34,33 @@ class Search(webapp2.RequestHandler):
     self.response.write(template.render(template_values))
 
 
+class AdminHandler(webapp2.RequestHandler):
+
+  def get(self):
+    template_values = {'upload_url': blobstore.create_upload_url('/upload')}
+    template = JINJA_ENVIRONMENT.get_template('upload.html')
+    self.response.write(template.render(template_values))
+
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+
+  def post(self):
+    upload_files = self.get_uploads('file')
+    blob_info = upload_files[0]
+    self.redirect('/serve/%s' % blob_info.key())
+
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+  def get(self, resource):
+    resource = str(urllib.unquote(resource))
+    blob_info = blobstore.BlobInfo.get(resource)
+    self.send_blob(blob_info)
+
+
 application = webapp2.WSGIApplication([
+    ('/serve/([^/]+)?', ServeHandler),
     ('/search', Search),
+    ('/upload', UploadHandler),
+    ('/admin', AdminHandler),
     ('/', MainPage),
 ], debug=True)
